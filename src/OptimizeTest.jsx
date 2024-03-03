07422e41a26d994da482515883671c185e11638a
Songminseok <songms0909@khu.ac.kr>
2024-03-03 13:00:28 +0900

    Object shallow compare 이해하기 - areEqual

--- src/OptimizeTest.jsx
+++ src/OptimizeTest.jsx
@@ -1,30 +1,49 @@
 import React, { useState, useEffect } from "react";
 
-const CountView = React.memo(({ count }) => {
+const CounterA = React.memo(({ count }) => {
+  useEffect(() => {
+    console.log(`CounterA Update - count : ${count}`);
+  });
   return <div>{count}</div>;
 });
 
-const TextView = React.memo(({ text }) => {
-  return <div>{text}</div>;
-});
+const CounterB = ({ obj }) => {
+  useEffect(() => {
+    console.log(`CounterB Update`);
+  });
+  return <div>{obj.count}</div>;
+};
+
+const areEqual = (prevProps, nextProps) => {
+  return prevProps.obj.count === nextProps.obj.count;
+};
+
+const MemoizedCounterB = React.memo(CounterB, areEqual);
 
 const OptimizeTest = () => {
   const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
+  const [obj, setObj] = useState({
+    count: 1,
+  });
   return (
     <div style={{ padding: 50 }}>
       <div>
-        <h2>Count</h2>
-        <CountView count={count} />
-        <button onClick={() => setCount(count + 1)}>+</button>
+        <h2>Counter A</h2>
+        <CounterA count={count} />
+        <button onClick={() => setCount(count)}>A button</button>
       </div>
       <div>
-        <h2>Text</h2>
-        <TextView text={text} />
-        <input
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        ></input>
+        <h2>Counter B</h2>
+        <MemoizedCounterB obj={obj} />
+        <button
+          onClick={() =>
+            setObj({
+              count: obj.count,
+            })
+          }
+        >
+          B button
+        </button>
       </div>
     </div>
   );
