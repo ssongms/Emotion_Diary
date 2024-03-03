2e33325856eff739d8157309488d4a40c7900db9
Songminseok <songms0909@khu.ac.kr>
2024-03-03 14:17:16 +0900

    useReducer : 상태 변화 로직 분리

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import { useState, useRef, useEffect, useMemo, useCallback } from "react";
+import { useRef, useEffect, useMemo, useCallback, useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      const created_date = new Date().getTime();
+      const newItem = {
+        ...action.data,
+        created_date,
+      };
+      return [newItem, ...state];
+    }
+    case "REMOVE": {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    case "EDIT": {
+      return state.map((it) =>
+        it.id === action.targetId ? { ...it, content: action.newContent } : it
+      );
+    }
+    default:
+      return state; // 상태 변화 X
+  }
+};
 
 function App() {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
+  const [data, dispatch] = useReducer(reducer, []);
   const dataId = useRef(0);
 
   useEffect(() => {
@@ -12,24 +39,16 @@ function App() {
   }, []);
 
   const onCreate = useCallback((author, content, emotion) => {
-    const created_date = new Date().getTime();
-    const newItem = {
-      author,
-      content,
-      emotion,
-      created_date,
-      id: dataId.current,
-    };
+    dispatch({ type: "CREATE", data: { author, content, emotion, id: dataId.current } });
     dataId.current += 1;
-    setData((data) => [newItem, ...data]); //원래 데이터에 new 일기를 이어붙임
   }, []);
 
   const onRemove = useCallback((targetId) => {
-    setData((data) => data.filter((it) => it.id !== targetId));
+    dispatch({ type: "REMOVE", targetId });
   }, []);
 
   const onEdit = useCallback((targetId, newContent) => {
-    setData((data) => data.map((it) => (it.id === targetId ? { ...it, content: newContent } : it)));
+    dispatch({ type: "EDIT", targetId, newContent });
   }, []);
 
   const getData = async () => {
@@ -45,7 +64,7 @@ function App() {
         id: dataId.current++,
       };
     });
-    setData(initData);
+    dispatch({ type: "INIT", data: initData });
   };
 
   const getDiaryAnalysis = useMemo(() => {
